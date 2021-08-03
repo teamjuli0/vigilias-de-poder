57274eba720ae132149ee8d011bb07259ee3fdd2
Julio Valdez <valdezjulio95@gmail.com>
2021-08-03 02:28:56 -0500

    updated navbar

--- client/src/views/home/index.js
+++ client/src/views/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Hero, Card, LargeCard } from '../../components/'
+import { Hero, Card, LargeCard, Navbar } from '../../components/'
 import './style.css'
 
 const imagesArr = [
@@ -15,79 +15,7 @@ const Home = () => (
   <>
     <main className='mainDiv'>
       <Hero />
-      <nav
-        style={{
-          width: '100%',
-          height: '80px',
-          position: 'sticky',
-          top: 0,
-          backgroundColor: '#171717',
-          zIndex: 4,
-          padding: 0,
-        }}
-      >
-        <div
-          style={{
-            maxWidth: '1150px',
-            margin: 'auto',
-            color: 'white',
-          }}
-        >
-          <nav class='navbar navbar-expand-lg navbar-dark'>
-            <div class='container-fluid'>
-              <a class='navbar-brand' href='#'>
-                Navbar
-              </a>
-              <button
-                class='navbar-toggler'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#navbarSupportedContent'
-                aria-controls='navbarSupportedContent'
-                aria-expanded='false'
-                aria-label='Toggle navigation'
-              >
-                <span class='navbar-toggler-icon'></span>
-              </button>
-              <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-                <ul class='navbar-nav ms-auto mb-2 mb-lg-0'>
-                  <li class='nav-item'>
-                    <a class='nav-link active' aria-current='page' href='#'>
-                      Home
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='#'>
-                      Link
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'start',
-              alignItems: 'center',
-              height: '40px',
-            }}
-          >
-            <p
-              style={{
-                height: '30px',
-              }}
-            >
-              SERMONS
-            </p>
-            <input
-              style={{
-                height: '30px',
-              }}
-            />
-          </div>
-        </div>
-      </nav>
+      <Navbar />
       <div className='flexRow'>
         <LargeCard />
         {imagesArr.map((image) => (
