2ba4e99bd7272e4fb8188aeb919bf961625f5853
Julio Valdez <valdezjulio95@gmail.com>
2021-08-03 02:35:03 -0500

    initial footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import { Navbar } from './components/'
+import { Footer } from './components/'
 import { Home } from './views/'
 import Spanish from './languages/es.json'
 
@@ -22,8 +22,8 @@ function App() {
         crossorigin='anonymous'
         referrerpolicy='no-referrer'
       />
-      {/* <Navbar buttonTxt={language.navbarButtons} /> */}
       <Home />
+      <Footer />
     </>
   )
 }
